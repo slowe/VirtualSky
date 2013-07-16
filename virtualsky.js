78abf256878c3296b77be48a8cbd72ead99f876d
Stuart <gravatar@strudel.org.uk>
2013-07-16 20:37:41 +0100

    Fix position of transparent link areas

--- virtualsky.js
+++ virtualsky.js
@@ -1103,8 +1103,7 @@ VirtualSky.prototype.fontsize = function(){
 	return (m < 500) ? ((m < 350) ? ((m < 300) ? ((m < 250) ? 9 : 10) : 11) : 12) : parseInt(this.container.css('font-size'));
 }
 VirtualSky.prototype.positionCredit = function(){
-	var off = this.container.position();
-	this.container.find('.'+this.id+'_credit').css({position:'absolute',top:off.top+parseFloat(this.tall)-5-this.fontsize(),left:off.left+5});
+	this.container.find('.'+this.id+'_credit').css({position:'absolute',top:parseFloat(this.tall)-5-this.fontsize(),left:5});
 }
 VirtualSky.prototype.updateSkyGradient = function(){
 	this.sky_gradient = this.ctx.createLinearGradient(0,0,0,this.tall);
@@ -1199,7 +1198,7 @@ VirtualSky.prototype.draw = function(proj){
 	if(this.container.find('.'+this.id+'_clock').length == 0){
 		this.container.append('<div class="'+this.id+'_clock" title="'+this.getPhrase('datechange')+'">'+clockstring+'</div>');
 		var off = $('#'+this.idinner).position();
-		this.container.find('.'+this.id+'_clock').css({position:'absolute',padding:0,width:metric_clock,cursor:'pointer',top:off.top+5,left:off.left+5,zIndex:20,display:'block',overflow:'hidden',backgroundColor:'transparent',fontSize:fontsize+'px',color:'transparent'}).bind('click',{sky:this},function(e){
+		this.container.find('.'+this.id+'_clock').css({position:'absolute',padding:0,width:metric_clock,cursor:'pointer',top:off.top+5,left:off.left+5,zIndex:20,display:'block',overflow:'hidden',backgroundColor:'transparent',fontSize:fontsize+'px',color:txtcolour}).bind('click',{sky:this},function(e){
 			var s = e.data.sky;
 			var id = s.id;
 			if($('#'+id+'_calendar').length == 0){
