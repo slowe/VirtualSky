6962ba67a3958e1181f78f562e7819b96dd51e05
Stuart <gravatar@strudel.org.uk>
2014-11-27 00:20:54 +0000

    Minor bug fix for broken language files to default to English

--- virtualsky.js
+++ virtualsky.js
@@ -974,6 +974,8 @@ VirtualSky.prototype.loadLanguage = function(l,fn){
 		},
 		function(){},
 		function(){
+			// If we tried to load the short version of the language and it failed, default to English
+			if(this.langshort==url){ this.loadLanguage('en',fn); }
 			if(url.indexOf(this.language) > 0) this.loadLanguage(this.langshort,fn);
 		}
 	);
