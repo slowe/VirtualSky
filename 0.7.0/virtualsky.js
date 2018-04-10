151635facd76ba1e9a14725e34c47d73ccb7ac2b
Stuart <gravatar@strudel.org.uk>
2018-04-10 17:49:48 +0100

    Testing

--- 0.7.0/virtualsky.js
+++ 0.7.0/virtualsky.js
@@ -1236,7 +1236,7 @@ VirtualSky.prototype.createSky = function(){
 		}).on('dblclick',{sky:this},function(e){
 			e.data.sky.debug('dblclick')
 			e.data.sky.toggleFullScreen();
-		}).on('mousemove touchmove',{sky:this},function(e){
+		}).on('mousemove',{sky:this},function(e){
 			e.preventDefault();
 			e.data.sky.debug('mousemove')
 			var s = e.data.sky;
@@ -1273,10 +1273,13 @@ VirtualSky.prototype.createSky = function(){
 				matched = s.whichPointer(x,y);
 				s.toggleInfoBox(matched);
 			}
-		}).on('mousedown touchdown',{sky:this},function(e){
+		}).on('touchmove',{sky:this},function(e){
+			e.preventDefault();
+			e.data.sky.debug('touchmove')
+		}).on('mousedown',{sky:this},function(e){
 			e.data.sky.debug('mousedown')
 			e.data.sky.dragging = true;
-		}).on('mouseup touchup',{sky:this},function(e){
+		}).on('mouseup',{sky:this},function(e){
 			e.data.sky.debug('mouseup')
 			var s = e.data.sky;
 			s.dragging = false;
