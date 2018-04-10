4289a3e309b9cfcd2983b529052e6669b8eb8b0f
Stuart <gravatar@strudel.org.uk>
2018-04-10 18:11:46 +0100

    Different x,y

--- 0.7.0/virtualsky.js
+++ 0.7.0/virtualsky.js
@@ -1276,8 +1276,11 @@ VirtualSky.prototype.createSky = function(){
 		}).on('touchmove',{sky:this},function(e){
 			e.preventDefault();
 			var s = e.data.sky;
-			var x = e.originalEvent.layerX;
-			var y = e.originalEvent.layerY;
+			var rect = e.target.getBoundingClientRect();
+			var x = e.targetTouches[0].pageX - rect.left;
+			var y = e.targetTouches[0].pageY - rect.top;
+			//var x = e.originalEvent.layerX;
+			//var y = e.originalEvent.layerY;
 			e.data.sky.debug('touchmove '+x+','+y+' '+s.x+','+s.y);
 			var theta,f,dr;
 			if(s.polartype){
